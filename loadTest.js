94a7bf2a34693159843f0f6dfe767c14522fbd5b
Saarika White <saarikak@gmail.com>
2022-01-15 11:47:51 -0800

    Add K6 local stress testing

--- loadTest.js
+++ loadTest.js
@@ -2,24 +2,39 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
+
 export default function () {
-  const rnd = Math.floor(Math.random() * 1000);
-  const response = http.get('http://localhost:3000/reviews');
+  const rnd = Math.floor(Math.random() * 1000012);
+  const response = http.get(`http://localhost:3000/reviews/meta?product_id=${rnd}`);
   check(response, {
-    "is status 200": (r) => r.status === 200,
-    "is id the same we asked": (r) => {
-      const id = Number(r.json("id"));
-      return id === rnd;
-    }
+    'is status 200': (r) => r.status === 200,
+    'transaction time < 50ms' : (r) => r.timings.duration < 50,
+    'transaction time < 100ms' : (r) => r.timings.duration < 100,
+    'transaction time < 200ms' : (r) => r.timings.duration < 200,
+    'transaction time < 500ms' : (r) => r.timings.duration < 500,
+    'transaction time < 1000ms' : (r) => r.timings.duration < 1000,
+    'transaction time < 2000ms' : (r) => r.timings.duration < 2000
   });
 }
 
 export let options = {
-  vus: 1,
-  duration: '5s',
-  // thresholds: {
-  //     'failed requests': ['rate<0.02'],
-  //     http_req_duration: ['p(95)<500'],
-  //     http_reqs: ['count>6000']
-  // },
+  vus: 100,
+  duration: '15s',
+  thresholds: {
+    http_req_failed: ['rate<0.01']
+  }
 };
+
+// export default function () {
+//   const rnd = Math.floor(Math.random() * 1000012);
+//   const response = http.get(`http://localhost:3000/reviews?product_id=${rnd}`);
+//   check(response, {
+//     'is status 200': (r) => r.status === 200,
+//     'transaction time < 50ms' : (r) => r.timings.duration < 50,
+//     'transaction time < 100ms' : (r) => r.timings.duration < 100,
+//     'transaction time < 200ms' : (r) => r.timings.duration < 200,
+//     'transaction time < 500ms' : (r) => r.timings.duration < 500,
+//     'transaction time < 1000ms' : (r) => r.timings.duration < 1000,
+//     'transaction time < 2000ms' : (r) => r.timings.duration < 2000
+//   });
+// }
